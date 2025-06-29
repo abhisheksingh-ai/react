ad761567e93bd9c3f989ba826975c67d7b2bdbef
Abhishek Singh <asingh19082000@gmail.com>
2025-06-29 14:34:37 +0530

    component with useState

--- episode03/App.js
+++ episode03/App.js
@@ -1,18 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 
-//element from core react 
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Heading from core React"
-)
-console.log("Core Heading", heading);
+function SumComponent() {
+    const [first, setFirst] = useState("")
+    const [second, setSecond] = useState("")
 
-//JSX heading
-const jsxHeading = <h1 id="jsxHeadingID">JSX Heading🤣</h1>
-console.log("JSX Heading", jsxHeading);
+    const getSum = (a, b) => {
+        const num1 = Number(a) || 0;
+        const num2 = Number(b) || 0;
+        return num1 + num2;
+    }
 
-//create root and render these headings
+    return (
+        <div>
+            <h1 id="sum">Enter Two Number</h1>
+            <input
+                placeholder="First Number"
+                value={first}
+                onChange={e => setFirst(e.target.value)}
+            />
+
+            <input
+                placeholder="Second Number"
+                value={second}
+                onChange={e => setSecond(e.target.value)}
+            />
+
+            <h1>Result is { getSum(first, second) }</h1>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(sum);
+root.render(< SumComponent />)
